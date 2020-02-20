44944d039c0d0d26685b99381c4788bb91a7e9be
ema_maia@hotmail.com <emamaia@hotmail.com>
2020-02-20 14:30:55 -0300

    string

--- strings/index.js
+++ strings/index.js
@@ -1,32 +1,38 @@
-const input="In the beginning God created the heavens and the earth. Now the earth was formless and empty, darkness was over the surface of the deep, and the Spiritof God was hovering over the waters.And God said, 'Let there be light', and there was light. God saw that the light was good, and he separated the light from the arkness. God called the light 'day', and the darkness he called 'night'. And there was evening, and there was morning - the first day."
+// import FileSaver from 'file-saver';
 
 
- const input_array = input.split(" ")
+const text="In the beginning God created the heavens and the earth. Now the earth was formless and empty, darkness was over the surface of the deep, and the Spiritof God was hovering over the waters.And God said, 'Let there be light', and there was light. God saw that the light was good, and he separated the light from the arkness. God called the light 'day', and the darkness he called 'night'. And there was evening, and there was morning - the first day."
 
- const lines = []
 
- let line =""
 
- console.log(input_array);
- 
- 
+function recebeTexto(input,compr){
+    const lines = []
+    let line =""
+   
+    const inputArray = input.split(" ") 
 
- input_array.forEach((item) =>{
-     if(line.length + item.length < 40){
-         if(line.length != 0){
-           line =  line.concat(" ");
-         }       
-       line =  line.concat(item);
-     } else{
-         console.log(line);         
-         lines.push(line);
-         line=item;
-     }
- })
+    inputArray.forEach((item) =>{
+        if(line.length + item.length < compr){
+            if(line.length != 0){
+              line =  line.concat(" ");
+            }       
+          line =  line.concat(item);
+        } else{
+            console.log(line);         
+            lines.push(line);
+            line=item;
+        }
+    })
+   
+    lines.push(line) 
+    console.log(line) 
+  
+    // var blob = new Blob(lines, {type: "text/plain;charset=utf-8"})
+    // FileSaver.saveAs(blob, "output.txt")
+ }
 
- lines.push(line)
+ recebeTexto(text,40)
 
- console.log(line);
  
  
 
